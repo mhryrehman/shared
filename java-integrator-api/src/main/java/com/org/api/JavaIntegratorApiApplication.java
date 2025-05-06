4e09f9e005d5c357e7372f47afa20b2e806fbf3d
yasir.rehman <yasir.rehman@curemd.com>
2025-05-06 17:55:42 +0500

    removing unusable code.

--- java-integrator-api/src/main/java/com/org/api/JavaIntegratorApiApplication.java
+++ java-integrator-api/src/main/java/com/org/api/JavaIntegratorApiApplication.java
@@ -8,23 +8,5 @@ public class JavaIntegratorApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaIntegratorApiApplication.class, args);
-		twoSum(new int[]{3, 3}, 6);
-	}
-	public static int[] twoSum(int[] nums, int target) {
-
-		int[] result = new int[2];
-
-		for(int i=0; i<nums.length; i++){
-
-			for(int j=1; j<nums.length; j++){
-				if(nums[i] + nums[j] == target){
-					result[0]=i;
-					result[1]=j;
-					break;
-				}
-			}
-
-		}
-		return result;
 	}
 }
